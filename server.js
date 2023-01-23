29b5ef214c5d7163e2a5c3025340f0eabdd9c6ba
dawntj44 <dawntj44@gmail.com>
2023-01-22 19:30:11 -0500

    updated tip calculator

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ app.engine("hedwig", (filePath, options, callBack) => {
   });
 });
 
+app.engine("hedwig", (filePath, options, callBack) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) return callBack(err);
+    const rendered = data
+      .toString()
+      .replace("#title#", `<title>${options.title}</title>`)
+      .replace("#message#", `<h1>${options.message}</h1>`)
+      .replace(
+        "#content#",
+        `<a href="http://localhost:3000/${options.content}">"click me on for gratituity only"</a>`
+      );
+    return callBack(null, rendered);
+  });
+});
+
 app.set("views", "./views");
 app.set("view engine", "hedwig");
 
@@ -27,29 +42,29 @@ app.get("/greeting", (request, response) => {
   });
 });
 
-app.get("/greeting/:Jacob", (req, res) => {
+app.get("/greeting/:name", (req, res) => {
   res.render("template", {
     title: "homework1",
     message: "Hello Jacob! So Great to See you!",
   });
 });
 
-// app.get("/tip", (req, res) => {
-//   res.render("template", {
-//     title: "homework1",
-//     message: "tip for service",
-//     content: "200",
-//   });
-// });
+app.get("/tip", (req, res) => {
+  res.render("template", {
+    title: "homework1",
+    message: "tip for service",
+    content: "200",
+  });
+});
 
-// app.get("/:money", (request, response) => {
-//   let total = request.params.money;
-//   response.render("template", {
-//     title: "homework1",
-//     message: `${total} bill for service`,
-//     content: `${total * 0.25} gratiuity`,
-//   });
-// });
+app.get("/:money", (request, response) => {
+  let total = request.params.money;
+  response.render("template", {
+    title: "homework1",
+    message: `${total} bill for service`,
+    content: `${total * 0.25} gratiuity`,
+  });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000");
